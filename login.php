13857991236fe265f04033bb0289167c27411f9c
Lucas Genovese <lucasgmcunha@gmail.com>
2025-09-16 22:05:50 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,9 +4,14 @@
  $senha = $_POST['senha'];
 
 
-$sql = "select nome from usuarios where cpf='$cpf' and senha ='$senha'";
+$sql = "select nome from usuarios where cpf = ? and senha = ?";
+if ($stmt = $conn ->prepare($sql)){
+    $stmt->bind_param("ss", $cpf, $senha);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
 
-if($resultado = $conn->query($sql)){
+
+
 
     $row = $resultado->fetch_assoc();
 
@@ -14,7 +19,7 @@ if($resultado = $conn->query($sql)){
 
 if(isset($row) && $row['nome'] != ''){
    session_start();
-   $_SESSION['nome'] = "fulano";
+   $_SESSION['nome'] = $row['nome'];
    $_SESSION['cpf'] = $_POST['cpf'];
    $_SESSION['senha'] = $_POST['senha'];
    header("Location: principal.php");
@@ -25,5 +30,5 @@ if(isset($row) && $row['nome'] != ''){
 }else{
     echo "erro no banco de dados";
 }
-
+$conn->close();
 ?>
